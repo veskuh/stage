69aa7a2838e2139ff538da9180ba7a186cd719e2
Vesa-Matti Hartikainen <vesku.h@gmail.com>
2013-08-04 18:36:37 +0300

    Add license and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+Stage
+=====
+Stage is a simple presentation program.
+
+Stage does not intend to be a full powerpoint replacement. It only will support few types of elements and properties. As we only have limited amount of functionality, to functionality should be intuitive and easy to use and learn.
+
+Status
+------
+early proof of concept of the editing functionality
+
+Author
+------
+Vesa-Matti Hartikainen 
+twitter: @veskuh
--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/*
+Stage - a simple presentation application
+Copyright (C) 2013 Vesa-Matti Hartikainen <vesku.h@gmail.com>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 #include <QApplication>
 #include <QQmlApplicationEngine>
 #include <QQuickWindow>
@@ -9,10 +28,10 @@ int main(int argc, char *argv[])
                                  .arg(QCoreApplication::applicationDirPath(), "qml/main.qml"));
     QObject *topLevel = engine.rootObjects().value(0);
     QQuickWindow *window = qobject_cast<QQuickWindow *>(topLevel);
-    if ( !window ) {
-        qWarning("Error: Your root item has to be a Window.");
+    if (!window) {
+        qWarning("Root has to be a Window");
         return -1;
-    } \
-    window->show(); \
+    }
+    window->show();
     return app.exec();
 }
